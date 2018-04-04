9de7c54c1f4580f89814d158ae968c48be83a06b
kryjash <kryjash@gmail.com>
2018-04-04 14:10:04 -0400

    TempForNow

--- app/src/main/java/com/googlemaps/jash/googlemaps3/MapsActivity.java
+++ app/src/main/java/com/googlemaps/jash/googlemaps3/MapsActivity.java
@@ -1,10 +1,21 @@
 package com.googlemaps.jash.googlemaps3;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -14,12 +25,19 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
+
     private GoogleMap mMap;
+    private LocationManager manager;
+    private LocationListener locationListener;
+
     //    ArrayList<String> CurrtLong = new ArrayList<String>();
 //    ArrayList<String> CurrtLat = new ArrayList<String>();
 //
@@ -79,6 +97,57 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        //request the location update thru location manager
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                //get the latitude and longitude from the location
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                //get the location name from latitude and longitude
+                Geocoder geocoder = new Geocoder(getApplicationContext());
+                try {
+                    List<Address> addresses =
+                            geocoder.getFromLocation(latitude, longitude, 1);
+                    String result = addresses.get(0).getSubLocality()+":";
+                    result += addresses.get(0).getLocality()+":";
+                    result += addresses.get(0).getCountryCode();
+                    LatLng latLng = new LatLng(latitude, longitude);
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(result));
+                    mMap.setMaxZoomPreference(20);
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
     }
 
     HashMap<Marker, MyMarkerData> mDataMap = new HashMap<>();
@@ -152,6 +221,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         float zoomLevel = (float) 12.0;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Hamilton, zoomLevel));
 
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        manager.removeUpdates(locationListener);
+        Log.i("onPause...","paused");
     }
 
 }
